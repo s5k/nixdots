131adc01cfc886c5e3b9ff4bf097298753adbb58
s5k <yungbloodx02@gmail.com>
2023-12-21 19:42:10 +0700

    minor fixs for tmux

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 
 {
-    programs.tmux = {
-        enable = true;
-        keyMode = "vi";
-        extraConfig = builtins.readFile ../../dotfiles/tmux/tmux.conf;
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    extraConfig = builtins.readFile ../../dotfiles/tmux/tmux.conf;
 
-        plugins = with pkgs; [
-            tmuxPlugins.sensible
-            tmuxPlugins.yank
-            tmuxPlugins.prefix-highlight
-        ];
-    };
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+      tmuxPlugins.prefix-highlight
+    ];
+  };
 }
