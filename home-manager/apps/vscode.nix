66b2a47478c6473eff3ba1912ca332063775f237
s5k <yungbloodx0@gmail.com>
2024-05-14 13:55:05 +0700

    add auto hide sidebar when focus on editor extension for vscode

--- home-manager/apps/vscode.nix
+++ home-manager/apps/vscode.nix
@@ -46,6 +46,12 @@
         version = "1.0.5";
         sha256 = "+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
       }
+      {
+        name = "auto-toggle-sidebar";
+        publisher = "dominicvonk";
+        version = "0.0.2";
+        sha256 = "dIarW3cbC6mdL0Gmito7e2D32AszUvNFyG4WFOxyahw=";
+      }
     ];
 
     keybindings = builtins.fromJSON (builtins.readFile ../../dotfiles/vscode/keybindings.json);
