5e6d238ef08fcb55c0b4a14290be08ed8b6494c4
s5k <40916972+s5k@users.noreply.github.com>
2024-08-13 13:59:55 +0700

    remove unused config

--- after_install.sh
+++ after_install.sh
@@ -14,18 +14,6 @@ sudo pacman -S docker docker-compose -y
 sudo systemctl enable docker.service
 sudo chmod 666 /var/run/docker.sock
 
-# Kmonad as non-root user
-sudo groupadd uinput
-sudo usermod -aG input $USER
-sudo usermod -aG uinput $USER
-
-echo "KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"" | sudo tee /etc/udev/rules.d/99-uinput.rules
-echo "KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"" | sudo tee /lib/udev/rules.d/99-uinput.rules
-
 # Add zsh to /etc/shells
 sudo sh -c "echo $(which zsh) >> /etc/shells"
 sudo chsh -s $(which zsh) $USER
-
-# install crontab
-sudo pacman -S cronie -y
-cat ~/Documents/nixdots/dotfiles/crontab/.cron | crontab -
