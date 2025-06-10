3aa8da7c564480fdb2ee8c2948d67181baf61082
s5k <40916972+s5k@users.noreply.github.com>
2025-06-10 22:04:25 +0700

    ✨ feat: enhance git configuration and add global .gitignore
    - Introduce a global .gitignore file to exclude common files and directories
    - Update git.nix to set default branch to "main" and configure excludesfile for git core settings

--- home-manager/apps/git.nix
+++ home-manager/apps/git.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, self, ... }:
 
 {
   programs.git = {
@@ -9,19 +9,22 @@
       "co" = ''checkout'';
     };
     extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
       url = {
         "ssh://git@github.com" = { insteadOf = [ "https://github.com" "gh" ]; };
-      };
-      url = { "ssh://git@gitlab.com" = { insteadOf = "https://gitlab.com"; }; };
-      url = {
+        "ssh://git@gitlab.com" = { insteadOf = "https://gitlab.com"; };
         "ssh://git@bitbucket.org" = { insteadOf = "https://bitbucket.org"; };
       };
       pull = { rebase = true; };
-      init = { defaultBranch = "master"; };
       rebase = { abbreviateCommands = true; autoStash = true; };
       push = { default = "current"; autoSetupRemote = true; };
       pack = { threads = 0; };
-      core = { pager = "${pkgs.delta}/bin/delta"; };
+      core = {
+        pager = "${pkgs.delta}/bin/delta";
+        excludesfile = "${self.outPath}/dotfiles/git/.gitignore.global";
+      };
       interactive = { diffFilter = "${pkgs.delta}/bin/delta --color-only"; };
       delta = {
         navigate = true;
